2dc23176f7b6075ff7cf842f0dad06a6b54c50cd
Nguyễn Diệu Anh <121100269+dieuanh2k4@users.noreply.github.com>
2025-09-17 21:34:00 +0700

    BE: Tạo model và các ràng buộc entity .NET

---
+++ Server/src/Models/Rooms.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.src.Models
+{
+    public class Rooms
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Capacity { get; set; } // sức chứa
+        public string? Status { get; set; }
+        public int TheaterId { get; set; }
+
+        public Theater? Theater { get; set; }
+        public Showtimes? Showtimes { get; set; }
+        public ICollection<Seats>? Seats { get; set; }
+    }
+}
---
+++ Server/src/Models/Seats.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.src.Models
+{
+    public class Seats
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Price { get; set; }
+        public string? Type { get; set; }
+        public int RoomId { get; set; }
+
+        public Rooms? Rooms { get; set; }
+    }
+}
