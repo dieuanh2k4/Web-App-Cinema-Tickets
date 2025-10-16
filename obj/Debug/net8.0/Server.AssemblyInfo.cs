8892255d546b2ee9bf54c80c1002be4f4ecd1774
AnHocCode <kidbuffalo064@gmail.com>
2025-10-16 17:29:13 +0700

    lognin/register

--- obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c3da9d9f8116bdc91d65e92bdd5b642ebc2fb1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4d9d5d7da9e334d7917f182f6c29edad7b2862d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
